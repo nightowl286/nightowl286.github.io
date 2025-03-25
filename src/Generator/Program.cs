7328152c425deab0ff43c5541341103d1fe0fb35
Nightowl <nightowl286@protonmail.com>
2025-03-25 11:48:54 +0000

    + Added generator project. * Updated .gitignore file.

---
+++ src/Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Generator;
+
+class Program
+{
+   #region Constants
+   private static readonly string BuildPath = Path.GetFullPath("../../../../../build");
+   #endregion
+
+   #region Functions
+   public static void Main()
+   {
+   }
+   #endregion
+}
